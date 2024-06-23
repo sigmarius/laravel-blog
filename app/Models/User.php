47583c2f7b6b4f7161b24d05740e2ad6558ef699
Anastasya <aamokeeva@yandex.ru>
2024-06-23 12:30:15 +0300

    Задействует отношения HasOne и BelongsTo

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,6 +4,8 @@ namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Relations\HasOne;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
@@ -43,5 +45,22 @@ class User extends Authenticatable
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
         ];
+    }
+
+    public function articles(): HasMany
+    {
+        return $this->hasMany(Article::class);
+    }
+
+    public function article(): HasOne
+    {
+//        return $this->hasOne(Article::class)->latestOfMany();
+
+        return $this->articles()->one();
+    }
+
+    public function profile(): HasOne
+    {
+        return $this->hasOne(UserProfile::class)->withDefault();
     }
 }
--- app/Models/UserProfile.php
+++ app/Models/UserProfile.php
@@ -6,14 +6,13 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
-class Article extends Model
+class UserProfile extends Model
 {
     use HasFactory;
 
     protected $fillable = [
         'user_id',
-        'title',
-        'text'
+        'address'
     ];
 
     public function user(): BelongsTo
